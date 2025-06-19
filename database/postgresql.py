e19e4a6aeb3cb740bbcf88d6804139ffaf216892
Bartosz <bmusial@protonmail.com>
2025-06-19 13:18:18 +0200

    feat(db): add PostgreSQLClient class with connection setup using environment variables

--- database/postgresql.py
+++ database/postgresql.py
@@ -1,3 +1,15 @@
+from dotenv import load_dotenv
 import psycopg2
+import os
 
-class PostgresClient:
+load_dotenv()
+
+class PostgreSQLClient:
+    def __init__(self):
+        self._conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port=os.getenv("DB_PORT")
+        )
