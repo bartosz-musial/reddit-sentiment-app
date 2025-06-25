abbf68cff1f1e58bc8259970f9eb6c23e617d20c
Bartosz <bmusial@protonmail.com>
2025-06-25 02:54:45 +0200

    feat(openrouter): add test_sentiment_model method and refactor openrouter/ for validating model avability

--- openrouter/openrouter_client.py
+++ openrouter/openrouter_client.py
@@ -2,7 +2,7 @@ import time
 from abc import ABC, abstractmethod
 from dotenv import load_dotenv
 from database.postgresql import PostgreSQLClient
-from openai import OpenAI
+from openai import OpenAI, AuthenticationError
 from openai.types.chat import ChatCompletionUserMessageParam
 import os
 import logging
@@ -38,13 +38,35 @@ class OpenRouter(ABC):
         """
         pass
 
-    def _analyze_sentiment(self, post_id: str, **kwargs):
-        prompt = self._build_prompt(**kwargs)
+    @staticmethod
+    def _test_prompt(**kwargs):
+        title = kwargs.get("title")
+        content = kwargs.get("content")
+        subreddit = kwargs.get("subreddit")
+
+        prompt = (
+            "Determine the sentiment of this Reddit post:"
+            f"1. Title: '{title}'"
+            f"2. Content: '{content}'"
+            f"3. Subreddit: '{subreddit}'"
+            "Respond with ONE WORD: POSITIVE, NEUTRAL, or NEGATIVE."
+        )
+
+        return prompt
+
+    @staticmethod
+    def _messages(prompt: str) -> list[ChatCompletionUserMessageParam]:
         messages: list[ChatCompletionUserMessageParam] = [
             {
                 "role": "user", "content": prompt
             }
         ]
+
+        return messages
+
+    def _analyze_sentiment(self, post_id: str, **kwargs):
+        prompt = self._build_prompt(**kwargs)
+        messages = self._messages(prompt)
 
         chat = self._client.chat.completions.create(
             model=self._model,
@@ -59,6 +81,30 @@ class OpenRouter(ABC):
             logging.warning(f"Invalid sentiment value returned: {sentiment}")
             self._storage.update_post_sentiment_invalid(post_id, self._model)
 
+    def _test_analyze_sentiment(self, prompt: str) -> str:
+        messages = self._messages(prompt)
+
+        chat = self._client.chat.completions.create(
+            model=self._model,
+            messages=messages,
+            temperature=self._temperature,
+            max_tokens=self._max_tokens
+        )
+        sentiment = chat.choices[0].message.content.strip().upper()
+
+        return sentiment
+
     @staticmethod
     def _sentiment_validation(sentiment: str) -> bool:
         return sentiment in ("POSITIVE", "NEUTRAL", "NEGATIVE")
+
+    def test_sentiment_model(self) -> bool:
+        post_id = self._storage.get_first_non_null_sentiment_record()
+        title, content, subreddit = self._storage.get_post_to_analyze(post_id)
+        prompt = self._test_prompt(title=title, content=content, subreddit=subreddit)
+        try:
+            self._test_analyze_sentiment(prompt)
+            return True
+        except AuthenticationError:
+            logging.warning(f"{self._model} not working properly!")
+            return False
