7dddf155ba7c4c8ee50194493b39c0af5ded1fe9
Bartosz <bmusial@protonmail.com>
2025-06-26 18:41:46 +0200

    feat(main): load scheduler and subreddit setting from config.yaml

--- main.py
+++ main.py
@@ -6,17 +6,34 @@ from logging_config.logging_config import get_config
 from dotenv import load_dotenv
 import time
 import os
-
-load_dotenv()
-get_config()
+import yaml
+import logging
 
 class MissingEnvFileError(Exception):
+    pass
+
+class MissingConfigFileError(Exception):
     pass
 
 if not os.path.exists(".env"):
     raise MissingEnvFileError("The .env file was not found!")
 
+if not os.path.exists("config.yaml"):
+    raise MissingConfigFileError("The config.yaml file was not found!")
+
+load_dotenv()
+get_config()
+
 def main():
+    with open("config.yaml") as f:
+        cfg = yaml.safe_load(f)
+
+    subreddits = cfg["subreddits"]
+    post_limit = cfg["post_limit"]
+
+    cron_post = CronTrigger(**cfg["cron_post"])
+    cron_sentiment = CronTrigger(**cfg["cron_sentiment"])
+
     llama = LlamaScout()
     mistral = MistralNemo()
 
@@ -26,11 +43,13 @@ def main():
         main_sentiment_model = mistral
 
     reddit = RedditClient.from_env()
+
     scheduler = BackgroundScheduler()
-    trigger_for_post = CronTrigger(hour="0-23", minute="0,30")
-    trigger_for_sentiment = CronTrigger(hour="0-23", minute="15,45")
-    scheduler.add_job(reddit.get_new_posts, trigger_for_post, args=["bitcoin", 5])
-    scheduler.add_job(main_sentiment_model.pipeline, trigger_for_sentiment)
+    for subreddit in subreddits:
+        scheduler.add_job(reddit.get_new_posts, cron_post, args=[subreddit, post_limit])
+        logging.info(f"Scheduled job 'get_new_posts' for subreddit r/{subreddit}")
+
+    scheduler.add_job(main_sentiment_model.pipeline, cron_sentiment)
     scheduler.start()
     try:
         while True:
